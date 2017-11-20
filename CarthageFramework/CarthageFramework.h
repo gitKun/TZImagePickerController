5967a2ee6cd6ecf631e657add322c0dd846dad5f
wuhaizeng <wuhaizeng@cmss.chinamobile.com>
2017-11-20 21:59:15 +0800

    carthage framework

---
+++ CarthageFramework/CarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarthageFramework.h
+//  CarthageFramework
+//
+//  Created by wuhaizeng on 20/11/2017.
+//  Copyright © 2017 谭真. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageFramework.
+FOUNDATION_EXPORT double CarthageFrameworkVersionNumber;
+
+//! Project version string for CarthageFramework.
+FOUNDATION_EXPORT const unsigned char CarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFramework/PublicHeader.h>
+
+
