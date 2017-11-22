3044c3cd94cb4ce9dfed98b86df76b4810d6b80f
wuhaizeng <wuhaizeng@cmss.chinamobile.com>
2017-11-22 10:35:16 +0800

    delete warning

---
+++ CarthageFramework/TZImagePickerControllerHeaders.h
@@ -0,0 +1,28 @@
+//
+//  TZImagePickerControllerHeaders.h
+//  TZImagePickerController
+//
+//  Created by wuhaizeng on 21/11/2017.
+//  Copyright © 2017 谭真. All rights reserved.
+//
+
+#ifndef TZImagePickerControllerHeaders_h
+#define TZImagePickerControllerHeaders_h
+
+#import <TZImagePickerController/TZPhotoPickerController.h>
+#import <TZImagePickerController/TZImagePickerController.h>
+#import <TZImagePickerController/TZAssetCell.h>
+#import <TZImagePickerController/TZAssetModel.h>
+#import <TZImagePickerController/TZPhotoPreviewController.h>
+#import <TZImagePickerController/TZPhotoPreviewCell.h>
+#import <TZImagePickerController/TZVideoPlayerController.h>
+#import <TZImagePickerController/TZGifPhotoPreviewController.h>
+#import <TZImagePickerController/TZGifPhotoPreviewController.h>
+#import <TZImagePickerController/TZProgressView.h>
+#import <TZImagePickerController/TZImageManager.h>
+#import <TZImagePickerController/TZImageCropManager.h>
+#import <TZImagePickerController/TZLocationManager.h>
+#import <TZImagePickerController/UIView+Layout.h>
+#import <TZImagePickerController/NSBundle+TZImagePicker.h>
+
+#endif /* TZImagePickerControllerHeaders_h */
